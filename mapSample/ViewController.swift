2997320b4ef60df42384edfa3e70a3559bcd3c2b
hiratsukashu <hiratsukashu@indival.co.jp>
2016-03-10 21:15:56 +0900

    Initial Commit

---
+++ mapSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapSample
+//
+//  Created by 平塚 俊輔 on 2016/03/10.
+//  Copyright © 2016年 平塚 俊輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
